1e74fe697d4697bcb68ded52a1be3d2a5af2fc42
Shivam <shs604813@gmail.com>
2022-12-09 20:17:33 +0530

    Adding Logical Buttons to TextUtils - 08

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,36 +4,62 @@ export default function Textform(props) {
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
-    // console.log("Up");
     // console.log(newText);
   };
+
+  const handleloClick = () => {
+    let newText = text.toLocaleLowerCase();
+    setText(newText);
+    // console.log(newText);
+  };
+
+  const handleClrClick = () => {
+    let newText = "";
+    setText(newText);
+  };
   const handleOnChange = (e) => {
-    // text = text.toUpperCase();
-    // console.log("OnChange");
     setText(e.target.value);
     console.log(text);
   };
 
-  const [text, setText] = useState("Enter Text Here...");
+  const [text, setText] = useState("");
   //   setText("Hey New Text");
   return (
-    <div className="container">
-      <div className="mb-3">
-        <label htmlFor="mybox" className="form-label">
-          <h3>
-            {props.heading} - {text}
-          </h3>
-        </label>
-        <textarea
-          className="form-control"
-          value={text}
-          id="mybox"
-          onChange={handleOnChange}
-          rows="3"></textarea>
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <label htmlFor="mybox" className="form-label">
+            <h3>{props.heading}</h3>
+          </label>
+          <textarea
+            className="form-control"
+            value={text}
+            id="mybox"
+            onChange={handleOnChange}
+            rows="8"></textarea>
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to UpperCase
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleloClick}>
+          Convert to LowerCase
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleClrClick}>
+          Clear
+        </div>
       </div>
-      <div className="btn btn-primary" onClick={handleUpClick}>
-        Convert to UpperCase
+
+      <div className="container my-4">
+        <h1>Your Text Summary</h1>
+        <p>
+          Your Word has {text.length} Character & {text.split(" ").length} Words
+        </p>
+        <p>You Took {0.008 * text.length} minutes to Read summary </p>
+        <h1>Preview</h1>
+        <p>
+          {text.length > 0 ? text : "Enter Some Thing in above box for Preview"}
+        </p>
       </div>
-    </div>
+    </>
   );
 }
