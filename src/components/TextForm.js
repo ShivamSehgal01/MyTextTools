35ffcdca2dc91845a79c5db042d9332e26119c42
Shivam <shs604813@gmail.com>
2022-12-10 17:25:33 +0530

    Adding More Buttons - Copy/pase Text - 09

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,29 +1,72 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
+  // Convert to UpperCase
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
     // console.log(newText);
   };
 
+  // Convert to LowerCase
   const handleloClick = () => {
     let newText = text.toLocaleLowerCase();
     setText(newText);
     // console.log(newText);
   };
 
-  const handleClrClick = () => {
+  //  Clear Text
+  function handleClrClick() {
     let newText = "";
     setText(newText);
+  }
+
+  // Copy Text
+  const handleCopy = () => {
+    let text = document.getElementById("mybox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
   };
+
+  // Paste Text
+  const handlePaste = () => {
+    let paste = document.getElementById("mybox");
+    navigator.clipboard.readText().then((text) => (paste.innerText = text));
+  };
+
+  // Remove Extra Spaces
+  const handleExtraSpace = () => {
+    let newT = text;
+    newT = text.split(/[ ]+/);
+    setText(newT.join(" "));
+  };
+
+  // Change Color
+  const handleColor = () => {
+    let box = document.getElementById("mybox");
+    let newColor = color;
+    if (box.style.color !== newColor) {
+      box.style.color = newColor;
+    } else {
+      box.style.color = "black";
+    }
+  };
+
+  // Handle Change
   const handleOnChange = (e) => {
     setText(e.target.value);
     console.log(text);
   };
 
+  //Handle Color
+  const handleOnColor = (event) => {
+    setColor(event.target.value);
+    console.log(color);
+  };
+
   const [text, setText] = useState("");
-  //   setText("Hey New Text");
+  const [color, setColor] = useState("");
+  //   setText("Hey New Text"); // Change State
   return (
     <>
       <div className="container">
@@ -38,6 +81,17 @@ export default function Textform(props) {
             onChange={handleOnChange}
             rows="8"></textarea>
         </div>
+
+        <h3>Enter Color to Change</h3>
+        <input
+          type="text"
+          placeholder="Enter your Color"
+          id="changeColor"
+          value={color}
+          className="form-control mt-2 mb-3"
+          onChange={handleOnColor}
+        />
+
         <div className="btn btn-primary mx-1" onClick={handleUpClick}>
           Convert to UpperCase
         </div>
@@ -45,7 +99,19 @@ export default function Textform(props) {
           Convert to LowerCase
         </div>
         <div className="btn btn-primary mx-1" onClick={handleClrClick}>
-          Clear
+          Clear Text
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handlePaste}>
+          Paste Text
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleExtraSpace}>
+          Remove ExtraSpace
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleColor}>
+          Change Color
         </div>
       </div>
 
